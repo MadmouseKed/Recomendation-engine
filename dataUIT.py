f2302a0621169d12780604b595177844adadb2af
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2021-03-16 11:13:26 +0100

    Initial commit

---
+++ dataUIT.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+gebruiker = "root"
+wachtwoord = ""
+dbNaam = "test"
+host = "localhost"
+
+#connectie naar mySQL
+def mysqlConnectie(gebruiker, wachtwoord, dbNaam, host):
+    """"
+    Verbinding met de gekozen mysql database.
+    :arg gebruiker ; Gebruikersnaam
+    :arg wachtwoord ; wachtwoord behorend bij gebruikersnaam
+    :arg dbNaam ; de naam van de database waarin gezoekt moet worden.
+    :arg host ; de link naar de server. Voor localhost gebruik ("localhost")
+    """
+    db = mysql.connector.connect(host=host, user=gebruiker, password=wachtwoord, database=dbNaam)
+    cursor = db.cursor()
+
+    return db, cursor
+
+def mysqlSluit(db, cursor):
+    cursor.close()
+    db.commit()
+    db.close()
+
+def mysql
