b56a967cee670401aef2e4331f92baf7abc77098
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2021-03-18 14:21:35 +0100

    Regelverwerking done
    Programma verwerkt de regels gegeven, creert een nieuwe dataset als resultaat.

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ import mysql
 def mysqlConnectie(loginInfo):
     """"
     Verbinding met de gekozen mysql database.
+    Voor eenvoud wordt de loginInfo in 1 lijst; [], meegegeven via loginInfo
     :arg gebruiker ; Gebruikersnaam
     :arg wachtwoord ; wachtwoord behorend bij gebruikersnaam
     :arg dbNaam ; de naam van de database waarin gezoekt moet worden.
@@ -63,39 +64,148 @@ def pakTables(cursor):
             tablesLijst.append(j)
     return tablesLijst
 
-def tabelInformatie(tabel, db, cursor):
-    print("Hoi")
-    resultaat = {}
+def leesColumns(cursor, tabelNaam):
+    tablesLijst = []
+    cursor.execute("SHOW COLUMNS FROM " +tabelNaam)
+    info = cursor.fetchall()
+    resultaat = []
+    for item in info:
+        resultaat.append(item[0])
+    return resultaat
 
+def tabelInformatie(tabel, cursor):
     cursor.execute("SELECT * FROM " + tabel)
     ophaal = cursor.fetchall()
 
     return ophaal
 
-def regelMaker():
-    prin("hoi")
+def regelMaker(cursor):
+    regel = []
 
 
+    return regel
 
-    return regels
+def regelMakerUI(cursor):
+    flag = True
+    while flag:
+        regelmakerUIInformatie()
+        check = True
+        while check:
+            invoer = input()
+            print(invoer)
+            print(type(invoer))
+            if invoer == '1':
+                print(pakTables(cursor))
+                check = False
+            elif invoer == '2':
+                print(2)
+                check = False
+            elif invoer == '3':
+                regelMaker(cursor)
+                check = False
+            elif invoer == '4':
+                flag = False
+                check = False
+            else:
+                print('Geen geldige input')
 
-def voorstel(regels, informatie):
-    pritn("hoi")
+def voorstelVerwerking(regel, data, columns, cursor):
+    """"
+    Hulpstuk voor voorstel(), verwerkt elke individuele regel naar wens.
+    """
+    resultaat = []
+    i = 0
+    columnID = ""
+    while i < len(columns) - 1:
+        if regel[0] in columns[i]:
+            columnID = i
+            break
+        i += 1
+
+    idLijst = []
+    keyLijst = tabelInformatie(regel[0], cursor)
+
+    for set in keyLijst:
+        if set[-1] in regel:
+            idLijst.append(set[0])
+
+    for product in data:
+        if product[columnID] in idLijst:
+            resultaat.append(product)
+
+    return resultaat
+
+def voorstel(regels, informatieTabel, cursor):
+    """"
+    Leest de regels uit en haalt de data waarover deze regels gaan besluiten op uit de informatieTabel,
+    vervolgens schrijft het een lijst van items die voldoen aan de regels.
+    :param regels bestaat uit een lijst van individuele regels. Elke regel wordt in volgorde van de lijst toegepast,
+    alleen items die aan alle regels voldoen blijven over in het einde.
+    :param informatieTabel
+    :param cursor
+    """
+    data = tabelInformatie(informatieTabel, cursor)
+    columns = leesColumns(cursor, informatieTabel)
+
     for regel in regels:
-        print(regel)
+        data = voorstelVerwerking(regel, data, columns, cursor)
+    return data
 
+def retrieve_tabel_data(table, cursor):
+    """
+    Maak een dictionary aan en execute een querry voor het opvragen van alle data in het table.
+    voor elke item in de lijst voeg de key en value toe aan een dictionary
+    :param table:
+    :param db:
+    :return:
+    """
+    # divine a dictonary
+    result_dict = {}
 
-def testCase():
+    # retrieve all items from selected table
+    cursor.execute("SELECT * FROM " + table)
+    myresult = cursor.fetchall()
+
+    # for each item in myresult
+    for item in myresult:
+        result_dict[item[1]] = item[0]
+    return result_dict
+
+def testCase(testcase):
     loginInfo = autoLogin()
     db, cursor = mysqlConnectie(loginInfo)
-    tabLijst = pakTables(cursor)
-    test = tabelInformatie("brand", db, cursor)
-    test2 = tabelInformatie("gender", db, cursor)
-    print(test)
-    print(test2)
 
-    print(tabLijst)
+    # tabLijst = pakTables(cursor)
+    # print(leesColumns(cursor, "GENDER"))
+    # test = tabelInformatie("brand", cursor)
+    # # test2 = tabelInformatie("gender", cursor)
+    # print(test)
+    # print(test2)
+    # regelMaker(cursor)
+    # voorstel(testcase, "products", cursor)
+
+    # a = "brand"
+    # b = "brand_id_key"
+    # if a in b:
+    #     print("True")
+    # else:
+    #     print("False")
+
+    # peanut = retrieve_tabel_data("products", cursor)
+    # print(peanut)
+    # print(peanut["Maya"])
+    # products = tabelInformatie("products", cursor)
+    # print(products[0])
+    # print(type(products[0]))
+    a = voorstel(testcase ,"products" ,cursor)
+    print(a)
+    # column = leesColumns(cursor, "products")
+    # print(column)
+
+
     mysqlSluit(db, cursor)
+    # show
 
-testCase()
+testcase = [["brand", "Maya", "Famosa", "Familie Pos"]]
 
+testCase(testcase)
